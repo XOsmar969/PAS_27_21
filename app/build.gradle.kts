d5e0503b41ee643473a32e5617eb0c15120f93e5
Maximillian Mikhael <max52634672@Gmail.com>
2025-05-26 09:43:23 +0700

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -40,9 +39,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation ("com.google.android.material:material:1.11.0")
-// Retrofit untuk HTTP requests
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    // Converter Gson untuk parsing JSON
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.picasso:picasso:2.71828")
 }
